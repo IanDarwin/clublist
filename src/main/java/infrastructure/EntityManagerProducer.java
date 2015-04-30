b4cc8a21c6ffe7f14410dedae4af7b7b33fe7bf5
Ian Darwin <ian@darwinsys.com>
2015-04-30 09:19:21 -0400

    EntityManagerProducer MUST be @Dependant. Grrr!

--- src/main/java/infrastructure/EntityManagerProducer.java
+++ src/main/java/infrastructure/EntityManagerProducer.java
@@ -1,5 +1,6 @@
 package infrastructure;
 
+import javax.enterprise.context.Dependent;
 import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
 import javax.persistence.EntityManager;
@@ -7,21 +8,26 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
 
 /**
- * Required for DeltaSpike Data
+ * "DeltaSpike Data requires an EntityManager exposed via a CDI producer -
+ * which is common practice in Java EE6 applications...
+ * This allows the EntityManager to be injected over CDI instead of 
+ * only being used with a @PersistenceContext annotation."
+ * @author Code from http://deltaspike.apache.org/documentation/data.html
  */
+@Dependent
 public class EntityManagerProducer {
+	
+	@PersistenceUnit
+	private EntityManagerFactory emf;
 
-    @PersistenceUnit
-    private EntityManagerFactory emf;
+	@Produces // you can also make this @RequestScoped
+	public EntityManager create() {
+		return emf.createEntityManager();
+	}
 
-    @Produces // you can also make this @RequestScoped
-    public EntityManager create() {
-        return emf.createEntityManager();
-    }
-
-    public void close(@Disposes EntityManager em) {
-        if (em.isOpen()) {
-            em.close();
-        }
-    }
+	public void close(@Disposes EntityManager em) {
+		if (em.isOpen()) {
+			em.close();
+		}
+	}
 }
